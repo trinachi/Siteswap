7eb25c31250d2f0da27c8f1aab1be1378385d3ca
trinachi <trinachi@gmail.com>
2012-02-17 10:40:51 -0600

    better test granularity

--- siteswap_spec.rb
+++ siteswap_spec.rb
@@ -1,75 +1,50 @@
 require 'siteswap'
-require 'input'
 
 describe Siteswap do
   describe "patterns" do
     it "gives me a pattern" do
-      @siteswap = Siteswap.new(2, nil)
-      @siteswap.patterns.should include([5, 1])
+      siteswap = Siteswap.new(2, nil)
+      siteswap.patterns.should include([5, 1])
+    end
+  end
+  
+  describe "correct_number?" do
+    it "makes sure the sum of throws / number of throws == number of objects" do
+      siteswap = Siteswap.new(2, 3)
+      siteswap.correct_number?(6).should be_true
+      siteswap.correct_number?(3).should_not be_true
+    end
+  end
+  
+  describe "good_first_throw" do
+    it "makes sure the first throw is high enough" do
+      siteswap = Siteswap.new(3, 3)
+      siteswap.good_first_throw([6, 2, 2]).should be_true
+      siteswap.good_first_throw([1, 2, 2]).should_not be_true
+    end
+  end
+  
+  describe "correct_timing" do
+    it "makes sure the juggler only catches one object at a time" do
+      siteswap = Siteswap.new(3, 3)
+      siteswap.correct_timing([5, 3, 1]).should be_true
+      siteswap.correct_timing([5, 4, 3]).should_not be_true
+    end
+  end
+  
+  describe "does_not_repeat" do
+    it "doesn't return multi-throw patterns where the base sequence repeats" do
+      siteswap = Siteswap.new(6, 3)
+      siteswap.does_not_repeat([3, 3, 3, 3, 3, 3]).should be_empty
+      # siteswap.does_not_repeat([4, 2, 3, 3, 2, 4]).should_not be_empty
+      # siteswap.does_not_repeat([4, 2, 3, 4, 2, 3]).should be_empty
     end
   end
     
   describe "valid_patterns" do
-    it "returns patterns where the sum of throws / number of throws = number of objects" do
-      @siteswap = Siteswap.new(2, 3)
-      @siteswap.valid_patterns.should include([4, 2], [5, 1])
-    end
-  
-    it "doesn't return patterns that don't match the number of objects" do
-      @siteswap = Siteswap.new(2, 2)
-      @siteswap.valid_patterns.should_not include([4, 2], [5, 1])
-    end
-    
-    it "returns valid throw/catch patterns" do
-      @siteswap = Siteswap.new(3, 3)
-      @siteswap.valid_patterns.should include([5, 3, 1])
-    end
-  
-    it "doesn't return invalid throw/catch patterns" do
-      @siteswap = Siteswap.new(3, 3)
-      @siteswap.valid_patterns.should_not include([5, 4, 3])
-    end
-  
-    it "doesn't return invalid throw/catch patterns" do
-      @siteswap = Siteswap.new(3, 3)
-      @siteswap.valid_patterns.should_not include([6, 1, 4])
-    end
-  
-    it "tests for other invalid throw/catch patterns" do
-      @siteswap = Siteswap.new(3, 3)
-      @siteswap.valid_patterns.should_not include([3, 1, 4])
-    end
-  
-    it "tests for different object and pattern length combinations" do
-      @siteswap = Siteswap.new(4, 4)
-      @siteswap.valid_patterns.should include([5, 5, 5, 1])
-    end
-    
-    it "doesn't return patterns that start with a throw height of 0 - 2" do
-      @siteswap = Siteswap.new(3, 3)
-      @siteswap.valid_patterns.should_not include([1, 2, 6])
-    end
-    
-    it "doesn't return multi-throw patterns where every number is the same" do
-      @siteswap = Siteswap.new(3, 3)
-      @siteswap.valid_patterns.should_not include([3, 3, 3])
-    end
-    
-    it "doesn't return multi-throw patterns where the base sequence repeats" do
-      @siteswap = Siteswap.new(4, 2)
-      @siteswap.valid_patterns.should_not include([3, 1, 3, 1])
-    end
-  end
-end
-
-describe Input do
-  describe "start" do
-    before(:each) do
-      @input = Input.new
-    end
-      
-    it "asks the user to enter the number of objects" do pending
-      @input.start.should include("Number of objects:")
+    it "returns valid patterns" do
+      siteswap = Siteswap.new(2, 3)
+      siteswap.valid_patterns.should include([4, 2], [5, 1])
     end
   end
 end
