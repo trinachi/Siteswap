a9070f8e16548ad15f724687fe412c3068f54271
trinachi <trinachi@gmail.com>
2012-02-21 16:25:21 -0600

    basic user interface

--- sinatra_app_runner.rb
+++ sinatra_app_runner.rb
@@ -5,14 +5,13 @@ require 'siteswap'
 @@siteswap = Siteswap.new(@pattern_length, @objects)
 
 get '/' do
-  html = ""
-  html << @@siteswap.values
-  return html
+  erb :pattern
 end
 
 post '/patterns' do
-  html = ""
+  # html = ""
   @@siteswap.get_values(params[:props], params[:length], params[:required], params[:excluded])
-  html << @@siteswap.all_patterns
-  return html
+  erb :valid_patterns
+  # html << @@siteswap.all_patterns
+  # return html
 end
