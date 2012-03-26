59fad61a1e8b81de1fc70481965705ee0c1c4d81
trinachi <trinachi@gmail.com>
2012-03-26 16:46:42 -0500

    improvements to js/css animator

--- public/js/juggling_animation.js
+++ public/js/juggling_animation.js
@@ -7,32 +7,41 @@ function findPattern() {
 		var pattern =  $.each(patterns, function(k, v) {
 			parseInt(v);
 		});
-		console.log(props);
-		console.log(pattern);
 		addListItems(pattern, props);
+	});
+};
+
+function thisQueue(i, pattern, props) {
+	var j = i;
+	$(this).delay(pattern[ j-1]*900).queue(function(next) {
+		$('.t' + j).removeClass('l' + pattern[ j-1]).addClass('r' + pattern[ j]);
+		// Figure out how to add this correct number from array for class -- timing issue
+		next();
 	});
 };
 
 function addListItems(pattern, props) {
 	$("#canvas").html("");
 
-for ( var i = 1; i <= props; i++ ) {
-	var listItem = $('<li>').attr('class', 't' + i);
-	if (i%2 === 0) listItem.addClass('r' + pattern[ i-1]); 
-	if (i%2 !== 0) listItem.addClass('l' + pattern[ i-1]);
-	if (pattern.length < i && i%2 === 0) listItem.addClass('r' + pattern[ i-(pattern.length + 1)]);
-	if (pattern.length < i && i%2 !== 0) listItem.addClass('l' + pattern[ i-(pattern.length + 1)]);
-	if (pattern.length*2 < i && i%2 === 0) listItem.addClass('r' + pattern[ i-(pattern.length*2 + 1)]);
-	if (pattern.length*2 < i && i%2 !== 0) listItem.addClass('l' + pattern[ i-(pattern.length*2 + 1)]);
-    $('#canvas').append(listItem);
-  }
-	$('#canvas > li').append('<div class="ball"></div>');
-};
-
-function changeClass(pattern, props) {
-	for ( var i = props + 1; i <= props*2; i++ ) {
-		$('#canvas > li').delay((i-props)*1000).removeClass('t' + (i - props)).addClass('t' + i);
+	for ( var i = 1; i <= props; i++ ) {
+		var listItem = $('<li>').attr('class', 't' + i);
+		
+		if (i%2 === 0) {
+			listItem.addClass('r' + pattern[ i-1]);
+		}
+		
+		if (i%2 !== 0) {
+		 	listItem.addClass('l' + pattern[ i-1]);
+			thisQueue(i, pattern, props);
+		}
+	
+		if (pattern.length < i && i%2 === 0) listItem.addClass('r' + pattern[ i-(pattern.length + 1)]);
+		if (pattern.length < i && i%2 !== 0) listItem.addClass('l' + pattern[ i-(pattern.length + 1)]);
+		if (pattern.length*2 < i && i%2 === 0) listItem.addClass('r' + pattern[ i-(pattern.length*2 + 1)]);
+		if (pattern.length*2 < i && i%2 !== 0) listItem.addClass('l' + pattern[ i-(pattern.length*2 + 1)]);
+	    $('#canvas').append(listItem);
 	}
+	$('#canvas > li').append('<div class="ball"></div>');
 };
 
 $(document).ready(function() {
